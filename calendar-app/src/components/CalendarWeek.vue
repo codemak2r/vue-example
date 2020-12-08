8adb20e7d23fe2d36f7a20bca1d8613ada68cd7f
AppSecurity Automation <john.wen@ehealth.com>
2020-12-08 20:24:15 +0800

    the second project

---
+++ calendar-app/src/components/CalendarWeek.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="calendar-week" class="container">
+        <div class="columns is-mobile">
+            <CalendarDay v-for="day in shareState.seedData" :key="day.id" :day="day"/>
+        </div>
+    </div>
+</template>
+<script>
+import { store } from "../store.js";
+import CalendarDay from "./CalendarDay.vue";
+
+export default {
+    name: "CalendarWeek",
+    data () {
+        return {
+            shareState: store.state
+        }
+    },
+    components: {
+        CalendarDay
+    }
+}
+</script>
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+</style>
---
+++ calendar-app/src/store.js
@@ -0,0 +1,6 @@
+import { seedData } from "./seed.js";
+export const store = {
+    state: {
+        seedData
+    }
+}
